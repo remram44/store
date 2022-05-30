9cf366bb22ed8c0dd212fa298ad04c763debd20e
Remi Rampin <remi@rampin.org>
2022-05-30 18:02:03 -0400

    Implement decryption

--- src/crypto.rs
+++ src/crypto.rs
@@ -11,7 +11,7 @@
 use aes::Aes128Enc;
 use aes::cipher::{BlockEncrypt, KeyInit};
 use aes::cipher::generic_array::GenericArray;
-use byteorder::{BigEndian, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
 use std::io::Cursor;
@@ -113,10 +113,10 @@ impl KeyPair {
     /// The function takes the current counter value, and returns the new
     /// value. That counter is used to prevent replay attacks; if the message
     /// countains a counter too low, it will be rejected.
-    pub fn decrypt(&self, data: &[u8], counter: u32) -> (Vec<u8>, u32) {
+    pub fn decrypt(&self, data: &[u8], min_counter: u32) -> Option<(Vec<u8>, u32)> {
         let mut result = Vec::new();
-        let counter = self.decrypt_into(data, &mut result, counter);
-        (result, counter)
+        let counter = self.decrypt_into(data, &mut result, min_counter);
+        counter.map(|c| (result, c))
     }
 
     /// Authenticate and decrypt some data
@@ -124,8 +124,72 @@ impl KeyPair {
     /// The function takes the current counter value, and returns the new
     /// value. That counter is used to prevent replay attacks; if the message
     /// countains a counter too low, it will be rejected.
-    pub fn decrypt_into(&self, data: &[u8], result: &mut Vec<u8>, counter: u32) -> u32 {
-        todo!()
+    pub fn decrypt_into(&self, data: &[u8], result: &mut Vec<u8>, min_counter: u32) -> Option<u32> {
+        result.clear();
+
+        if data.len() < 4 + SIZE + MAC_SIZE {
+            eprintln!("decrypt: missing MAC (size={})", data.len());
+            return None;
+        }
+        if data.len() % SIZE != (4 + MAC_SIZE) % SIZE {
+            eprintln!("decrypt: wrong size ({})", data.len());
+            return None;
+        }
+
+        // Check MAC
+        let mut mac = <Hmac::<Sha256> as Mac>::new_from_slice(&self.mac_key).unwrap();
+        mac.update(&data[0..data.len() - MAC_SIZE]);
+        match mac.verify_slice(&data[data.len() - MAC_SIZE..]) {
+            Ok(()) => {}
+            Err(_) => {
+                eprintln!("Invalid MAC");
+                return None;
+            }
+        }
+
+        // Read counter
+        let mut counter = Cursor::new(&data).read_u32::<BigEndian>().unwrap();
+        if counter < min_counter {
+            eprintln!("Invalid counter");
+            return None;
+        }
+
+        // Initialize cipher
+        let cipher = Aes128Enc::new(&GenericArray::from(self.encrypt_key.clone()));
+
+        // Prepare first block
+        let mut block = [0u8; SIZE];
+        block.clone_from_slice(&data[4..4 + SIZE]);
+
+        // Decrypt
+        xor_block(&mut block, &cipher_block(&cipher, counter));
+        counter += 1;
+
+        // Read total length
+        let mut length = Cursor::new(&block).read_u32::<BigEndian>().unwrap() as usize;
+        let copy_len = length.min(SIZE - 4);
+        result.extend_from_slice(&block[4..4 + copy_len]);
+        length -= copy_len;
+        let mut pos = 4 + SIZE;
+
+        // Do other blocks
+        while length > 0 {
+            let mut block = [0; SIZE];
+            block.clone_from_slice(&data[pos..pos + SIZE]);
+            pos += SIZE;
+            xor_block(&mut block, &cipher_block(&cipher, counter));
+            counter += 1;
+            let copy_len = length.min(SIZE);
+            result.extend_from_slice(&block[0..copy_len]);
+            length -= copy_len;
+        }
+
+        if data.len() - pos != MAC_SIZE {
+            eprintln!("Invalid size left over");
+            return None;
+        }
+
+        Some(counter)
     }
 }
 
@@ -180,5 +244,47 @@ mod tests {
             result,
             expected,
         );
+    }
+
+    #[test]
+    fn test_decrypt() {
+        let key_pair = KeyPair {
+            mac_key: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            encrypt_key: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32],
+        };
+
+        let mut ciphertext = Vec::new();
+        // Initial counter (4)
+        ciphertext.extend_from_slice(&[0, 0, 0, 4]);
+        // Encrypted data
+        ciphertext.extend_from_slice(b"\
+            \x6c\x25\xf2\x89\x66\xb2\x4b\x30\x72\x96\xf5\xb6\x76\xdc\x76\x41\
+            \x16\xda\x5a\x77\x54\xee\xc3\x2c\x59\x09\xe4\x2f\x7c\x95\x4e\xf0\
+            \xe5\xa7\xbc\xed\x59\x42\xdb\x7c\xcf\x63\x6a\x01\x98\x18\x73\xce\
+            \x69\x36\x8c\x4a\xb5\x7c\xe3\xfb\x8d\xc6\x78\x68\x3b\x4a\x18\xde\
+            \x82\x16\x2d\x5a\x38\xb9\xa4\x13\x17\x68\xf7\x16\xe0\x12\x7b\x60\
+            \xde\x82\x8a\x0c\x31\x58\x19\x8e\x62\xa8\xa8\xc6\x4b\x72\xb1\xbb\
+            \xf8\x77\xff\xcf\xa2\xf7\xa1\x21\xb7\xa5\x8e\x64\x8b\x5f\xe5\x6b\
+            \x49\xf9\x14\xc8\xb5\x4d\x6e\x1a\x87\xb6\x27\x65\xf6\x8c\xfe\x33\
+            \xc9\x4a\x25\xeb\x9b\x15\xc5\xb8\x6b\xd0\x1f\x60\xc2\x84\x33\x4b\
+            \xd3\x43\xbb\x76\xda\x05\x53\xb2\x3c\x0f\x6f\x4c\x34\x7c\x4c\xbd\
+            \x57\x90\x60\xf7\xbe\x1f\x0f\xa4\x7d\xc4\xb2\x5d\x88\x59\x37\x60\
+            \x4e\x11\x9f\x0e\x77\xbf\x1f\xb1\x5a\xc9\xed\x3f\xde\xdc\xf4\x07\
+            \x6c\xec\xbd\xa9\xe8\x7d\x8f\xfe\x81\x78\xa4\xdf\x4a\xc9\x6d\x49\
+            \xdc\x15\x11\x95\x68\x40\xde\x9b\x6e\xe9\x1b\xc2\xda\xe4\x74\x2b");
+        // MAC
+        ciphertext.extend_from_slice(b"\
+          \xf5\x4d\x3c\xa0\x76\x5d\xef\xab\x12\x5b\xe1\x6f\x62\x6b\x85\x20\
+          \x82\x50\xc5\x55\x89\xe4\x13\xc0\x86\x1a\x8c\xf4\x2d\xa7\x3f\xd4");
+
+        let (result, counter) = key_pair.decrypt(&ciphertext, 3).unwrap();
+        assert_eq!(counter, 18);
+
+        let message = b"\
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas \
+            est purus, sagittis eu cursus sed, ullamcorper sed nibh. Mauris \
+            quis aliquam leo. Integer porttitor sapien orci, sed semper ex \
+            elementum maximus.";
+        assert_eq!(result, message);
     }
 }
